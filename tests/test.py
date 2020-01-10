d1c8c04d4475106469558b035db31da92049b1ea
Marcus <marcdh@uga.edu>
2020-01-10 16:41:34 -0500

    Merge failing code test complete

--- tests/test.py
+++ tests/test.py
@@ -1,14 +1,8 @@
 import unittest
 
-#import coveralls
-
-#coveralls.wear()
 class DepTest(unittest.TestCase):
     def test_pass(self):
         self.assertTrue(True)
-
-    def test_pass(self):
-        self.assertTrue(False)
 
 if __name__ == "__main__":
     unittest.main()
