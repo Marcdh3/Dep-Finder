2160c0fa64dda9379f2b71a4f9bca88113fc0706
Marcus <marcdh@uga.edu>
2020-01-10 14:38:39 -0500

    Added an intentional test to fail

--- tests/test.py
+++ tests/test.py
@@ -7,5 +7,8 @@ class DepTest(unittest.TestCase):
     def test_pass(self):
         self.assertTrue(True)
 
+    def test_pass(self):
+        self.assertTrue(False)
+
 if __name__ == "__main__":
     unittest.main()
