91637a5f294687e4704ebcfaf29fa6e32840b701
Marcus <marcdh@uga.edu>
2020-01-10 13:40:39 -0500

    Removed the intentional fail test

--- tests/test.py
+++ tests/test.py
@@ -3,9 +3,6 @@ import unittest
 class DepTest(unittest.TestCase):
     def test_pass(self):
         self.assertTrue(True)
-    
-    def test_fail(self):
-        self.assertTrue(False)
 
 if __name__ == "__main__":
     unittest.main()
