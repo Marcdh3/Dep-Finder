d020b93ec0fd6531f76dbe10d81e178261c6e2c8
Marcus <marcdh@uga.edu>
2020-01-12 00:30:57 -0500

    Cleaned up finder tests

--- tests/test_finder.py
+++ tests/test_finder.py
@@ -1,12 +1,12 @@
 import unittest
 
-from depfinder import finder as df
+import depfinder.finder as df
 
 class FinderTest(unittest.TestCase):
     def test_find_deps(self):
-        finder = df.find_deps('depfinder')
+        deps = df.find_deps('depfinder')
         ground_truth = ['coverage', 'chardet', 'idna', 'urllib3', 
                           'requests', 'docopt', 'coveralls']
-        finder.sort()
+        deps.sort()
         ground_truth.sort()
-        self.assertEqual(finder, ground_truth)
+        self.assertEqual(deps, ground_truth)
