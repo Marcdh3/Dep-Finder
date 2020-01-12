718261eb8276432dcd52acc0af4c29ca99a7d8ec
Marcus <marcdh@uga.edu>
2020-01-11 21:43:04 -0500

    Added an extra line to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setuptools.setup(
     version='0.1',
     packages=['depfinder'],
     package_dir={'depfinder':'src'},
+    include_package_data=True,
     install_requires=[
         'coverage',
     ])
