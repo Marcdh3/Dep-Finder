f78673a2f5cd7bc4f6741fea9da1ae9c0a316dd1
Marcus <marcdh@uga.edu>
2020-01-11 21:52:05 -0500

    Added a blank file for a Codecov test

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setuptools.setup(
     version='0.1',
     packages=['depfinder'],
     package_dir={'depfinder':'src'},
-    include_package_data=True,
     install_requires=[
         'coverage',
     ])
